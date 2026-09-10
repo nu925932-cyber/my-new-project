58d68de31e38a684b5cccca47b9ce02cba228226
nu925932-cyber <nu925932@gmail.com>
2026-09-10 11:16:49 +0300

    add requirements.txt and refactor test

--- refactor_test.py
+++ refactor_test.py
@@ -1,10 +1,10 @@
-x = 10
-print(x)
-y = x + 5
+items_count = 10
+print(items_count)
+y = items_count + 5
 print(y)
 
 def calc():
-    return x * 2
+    return items_count * 2
 
 result = calc()
-print(result, x)
+print(result, items_count)
